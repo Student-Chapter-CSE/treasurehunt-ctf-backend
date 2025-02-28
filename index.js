7088a8ff5bf9859a380c91bb59dd7b59f43fe9b1
subha03 <roysubhajit2003@gmail.com>
2025-02-28 16:12:39 +0530

    done login model

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const connection=require("./connection")
 connection.connectMongodb('mongodb://127.0.0.1:27017/treasure_hunt')
 //const port = process.env.PORT || 4000;
 const port=4000;
-const signuprouter=require("./routes/route")
+const signuprouter=require("./routes/signup")
+const loginuser=require("./routes/login")
 app.use(express.urlencoded({ extended: false }));
 app.use("/",signuprouter)
-
+app.use("/login",loginuser)
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
