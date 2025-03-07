8633d8c195257c05950b1bd762ba848e8798824e
subha03 <roysubhajit2003@gmail.com>
2025-03-07 14:56:52 +0530

    added controller function for admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,46 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const adminmodel = require('../models/admin');
 const Ban = require('../models/Ban');
 const usermodel = require('../models/user');
 const scoresmodel = require('../models/scores');
-
-router.get('/', (req,res)=>{
-    res.json({mes: 'admin page'})
-
-})
-
-router.post('/getuser', async(req, res)=>{
-    try{
-        let user = await usermodel.find();
-        let score =  await scoresmodel.find();
-        res.status(200).render('admin', {user, score});
-    }catch(err){
-        console.error(err.message);
-    }
-})
-
-router.post("/ban", async (req, res) => {
-  const { Team_Name } = req.body;
-  if (!Team_Name) return res.status(400).send("Team_Name is required");
-
-  try {
-    await Ban.create({ Team_Name });
-    res
-    .status(200)
-    .redirect('/login');
-  } catch (error) {
-    res.status(500).send("Error banning.User might have been already banned.");
-  }
-});
-
-router.post("/unban", async (req, res) => {
-  const { Team_Name } = req.body;
-  if (!Team_Name) return res.status(400).send("Team_Name is required");
-
-  await Ban.deleteOne({ Team_Name });
-  res.status(200).send(` ${Team_Name} has been unbanned. `);
-});
-
-
+const { getUser , getAdminPage, banUser, unbanUser}=require("../controller/admin");
+router.get('/',getAdminPage);
+router.get('/getuser',getUser)
+router.post("/ban",banUser);
+router.delete("/unban", unbanUser);
 module.exports = router;
